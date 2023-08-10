8a8aa075478e0df8e1402a2512b0f458a267c5cb
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-10 17:06:04 -0300

    Update counter.js
    Added header.

--- js/counter.js
+++ js/counter.js
@@ -14,6 +14,7 @@ function hit()
   
   const query = new XMLHttpRequest();
   query.addEventListener("load", u);
+  query.setRequestHeader("Authorization", token)
   query.open("GET", "https://api.baserow.io");
   query.send();
 }
