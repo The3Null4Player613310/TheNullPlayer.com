ddf126e08db4e394b5df724ee07f624ae6043984
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-11 01:26:34 +0000

    Update counter.js
    Updated counter.

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 const token="andDcmxuUFFOWW4xeVFTU1pCZE5ybVZtYVNWYnZzTU0=";
-const domain="https://api.baserow.io/";
+const domain="https://api.baserow.io/api";
 const t_id="187609"
 const s_id="1";
 
@@ -17,7 +17,7 @@ function hit()
   $.ajax({
     dataType: "json",
     url:  domain,
-    headers: { Authentication:atob(token) },
+    headers: { Authentication:"Token " + atob(token) },
     //data: data,
     success: s
   });
