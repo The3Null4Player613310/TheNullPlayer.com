f650d23c1fd5b6a317d623796a4dca086afc4ae2
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-11 01:56:21 +0000

    Update counter.js
    Updated query url.

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,7 @@
 const token="andDcmxuUFFOWW4xeVFTU1pCZE5ybVZtYVNWYnZzTU0=";
 const domain="aHR0cHM6Ly9hcGkuYmFzZXJvdy5pby9hcGkv";
 const t_id="187609";
-const s_id="0";
+const s_id="1";
 
 function hit()
 {
