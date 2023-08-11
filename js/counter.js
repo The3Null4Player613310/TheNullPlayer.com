fb158183a74b644bb7bd71e9b6ae6ca58f44b4b7
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-11 00:52:22 +0000

    Update counter.js
    Updated query.

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,7 @@
 const token="andDcmxuUFFOWW4xeVFTU1pCZE5ybVZtYVNWYnZzTU0=";
 const domain="aHR0cHM6Ly9hcGkuYmFzZXJvdy5pby9hcGkv";
+const id = 1;
+
 
 function hit()
 {
@@ -10,7 +12,7 @@ function hit()
   
   $.ajax({
     dataType: "json",
-    url: atob(domain),
+    url: atob(domain) + "database/rows/table/187609/" + id + "/",
     headers: { Authentication:atob(token) },
     //data: data,
     success: s
