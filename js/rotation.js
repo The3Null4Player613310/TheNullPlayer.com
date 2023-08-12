366c95a6d6eea7cf1476ca49e6df28397e4ffd1d
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-12 02:50:11 -0300

    Update rotation.js
    Added rotation lock code.

--- js/rotation.js
+++ js/rotation.js
@@ -3,4 +3,13 @@ function rotation_lock()
   screen.orientation.lock("portrait");
 }
 
-rotation_lock();
+function rotation_check()
+{
+  const UA = navigator.userAgent;
+  return ( UA.includes("iPhone") || UA.includes("iPod") || UA.includes("IEMobile") || UA.includes("Android") || UA.includes("Windows Phone") );
+}
+
+if(rotation_check())
+{
+  rotation_lock();
+}
