48e59b1bbc06e80d700908c3360677c08485fbe7
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-13 21:47:05 -0300

    Update feed.js
    Updated feed.js

--- js/feed.js
+++ js/feed.js
@@ -1,4 +1,17 @@
+const URL = "http://stackoverflow.com/feeds/user/7305052"
+
 function feed()
 {
-  document.getElementById("counter").innerText = "feed";
+  function s(r)
+  {
+    document.getElementById("counter").innerText = "feed";
+  }
+
+  $.ajax({
+    dataType: "xml",
+    url: URL,
+    accepts: { xml:"application/rss+xml" },
+    success: s
+  });
+  
 }
