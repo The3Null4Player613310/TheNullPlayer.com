6ccc8cab3bd9de7c09494fe856d6adc4b6428582
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-15 09:35:50 -0300

    Update drawer.js
    Added js code.

--- js/drawer.js
+++ js/drawer.js
@@ -6,8 +6,12 @@ function drawer()
 
 function open()
 {
+  $(nav-drawer).setAttribute("class", "nav-drawer-open");
+  $(nav-shim).setAttribute("class", "nav-drawer-shim-open");
 }
 
 function close()
 {
+  $(nav-drawer).setAttribute("class", "nav-drawer-close");
+  $(nav-drawer).setAttribute("class", "nav-drawer-shim-close");
 }
