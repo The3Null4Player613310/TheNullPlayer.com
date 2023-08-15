6595cc253d6b5b4fd2979a42857eab43a3d0c429
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-15 09:44:25 -0300

    Update drawer.js
    Added basic drawer.js

--- js/drawer.js
+++ js/drawer.js
@@ -6,12 +6,20 @@ function drawer()
 
 function open()
 {
-  $(nav-drawer).setAttribute("class", "nav-drawer-open");
-  $(nav-shim).setAttribute("class", "nav-drawer-shim-open");
+  if(isOpen === false)
+  {
+    $("#nav-drawer").attr("class", "nav-drawer-open");
+    $("#nav-drawer-shim").attr("class", "nav-drawer-shim-open");
+    isOpen = true;
+  }
 }
 
 function close()
 {
-  $(nav-drawer).setAttribute("class", "nav-drawer-close");
-  $(nav-drawer).setAttribute("class", "nav-drawer-shim-close");
+  if(isOpen === true)
+  {
+    $("#nav-drawer").attr("class", "nav-drawer-close");
+    $("#nav-drawer-shim").attr("class", "nav-drawer-shim-close");
+    isOpen = false;
+  }
 }
