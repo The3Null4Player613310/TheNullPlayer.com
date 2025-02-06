0a91a74dda946b01a05feffefee9de2f14844e40
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2025-02-06 22:25:58 +0000

    Create player.js
    Added player.js

---
+++ js/player.js
@@ -0,0 +1,7 @@
+/*
+################################################################
+#TheNullPlayer: player.js
+#Copyright © 2024 Allison Munn
+#FULL COPYRIGHT NOTICE IS IN README
+################################################################
+*/
