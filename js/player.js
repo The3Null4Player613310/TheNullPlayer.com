a8b899ec819e6a8176399bbe6a838e272c7ed9f5
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2025-02-06 22:29:39 +0000

    Update player.js
    Modified player.js

--- js/player.js
+++ js/player.js
@@ -5,3 +5,27 @@
 #FULL COPYRIGHT NOTICE IS IN README
 ################################################################
 */
+
+function player()
+{
+    var stream;
+      function onYouTubeIframeAPIReady() {
+        stream = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+}
