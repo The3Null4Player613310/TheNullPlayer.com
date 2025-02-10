0f4fbeffdc1412818cdcc2a9ce0942651f5a25ff
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2025-02-10 02:40:15 +0000

    Update timer.js
    Updated timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,31 @@
+function d(tIn)
+{
+    function s(cb)
+    {
+        function ti()
+        {
+            cb(Number(tIn)-1);
+        }
+        
+        setTimeout(ti, 1000);
+    }
+    
+    return new Promise(s);
+}
+
+async function countDown()
+{
+  var t = Number("15");
+  while(t != 0)
+  {
+    t = Number(await d(t));
+    
+  }
+
+}
+
 function timer()
 {
-    var timer = document.getElementById("timer");
-    timer.innerHTML = "0";
+  var timer = document.getElementById("timer");
+  timer.innerHTML = "0";
 }
